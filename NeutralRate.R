6177d609484dbda979d563d74ac4ebb6b86e5eb3
HUGO LINDELL <hugolindell@gmail.com>
2020-04-06 08:37:33 +0200

    Changed neutral rate.

--- NeutralRate.R
+++ NeutralRate.R
@@ -1,8 +1,8 @@
 NeutralRate <- function(time_period)
 {
   
-  if (time_period < 10)
-    return(- 0.02 + (0.04 / 10 * time_period))
+  if (time_period < 11)
+    return(- 0.02 + (0.04 / 10 * (time_period-1)))
   else
     return(0.02)
 }
